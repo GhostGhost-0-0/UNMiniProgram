2121e5b73e3d18f4b21b05bb4900e16020d0a98f
PermabanSun <295375895@qq.com>
2022-06-01 15:04:19 +0800

    增加了更新动态点赞数的枚举类型

--- un-framework/src/main/java/com/zzx/enums/AppHttpCodeEnum.java
+++ un-framework/src/main/java/com/zzx/enums/AppHttpCodeEnum.java
@@ -14,7 +14,9 @@ public enum AppHttpCodeEnum {
     NO_OPERATOR_AUTH(403,"无权限操作"),
     SYSTEM_ERROR(500,"出现错误"),
     FILE_NULL_ERROR(501,"文件不能为空"),
-    CONTENT_NULL_ERROR(502,"内容不能为空");
+    CONTENT_NULL_ERROR(502,"内容不能为空"),
+    LOGIN_ERROR(505,"登录失败"),
+    COMMUNITY_NULL_ERROR(506,"动态不存在，无法更新");
 
     int code;
     String msg;
